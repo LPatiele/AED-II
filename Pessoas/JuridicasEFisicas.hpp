336e737902d2ccbcdda5ad8403dfae331266ae24
LPatiele <lo.patiele@gmail.com>
2016-04-13 22:03:33 -0300

    aula 5

--- Pessoas/JuridicasEFisicas.hpp
+++ Pessoas/JuridicasEFisicas.hpp
@@ -6,18 +6,20 @@ using namespace std;
 class Pessoa{
 
   protected:
-    string nome;
-    int idade;
+
     Pessoa * filhoEsquerda;
     Pessoa * filhoDireita;
+    Pessoa * pai;
 
   public:
-
+    string nome;
+    int idade;
     Pessoa(){
       nome = "";
       idade = 0;
       filhoDireita = NULL;
       filhoEsquerda = NULL;
+      pai = NULL;
     };
 
     Pessoa( string n, int id){
@@ -25,6 +27,7 @@ class Pessoa{
       idade = id;
       filhoDireita = NULL;
       filhoEsquerda = NULL;
+      pai = NULL;
     };
 
     ~Pessoa(){
@@ -48,48 +51,94 @@ class Pessoa{
     // };
 
     void inserirPessoa(Pessoa *nova){
-
       if (nova->idade < this->idade){
-
-        if (this->filhoEsquerda == NULL)
+        if (this->filhoEsquerda == NULL) {
           this->filhoEsquerda = nova;
-        else
-          this->filhoEsquerda->inserirPessoa(nova);
-
+          nova->pai = this;
+        }
+        else  this->filhoEsquerda->inserirPessoa(nova);
       }else{
-
-        if (this->filhoDireita == NULL)
+        if (this->filhoDireita == NULL) {
           this->filhoDireita = nova;
-        else
-          this->filhoDireita->inserirPessoa(nova);
+          nova->pai = this;
+        }
+        else  this->filhoDireita->inserirPessoa(nova);
+      }
+    };
+
+    void mostrarPre(){
+      cout << this->nome << " : " << this->idade  << endl;
+      if (filhoEsquerda != NULL)  this->filhoEsquerda->mostrarPre();
+      if(filhoDireita != NULL)  this->filhoDireita->mostrarPre();
+    };
+
+    void mostrarOrdem(){
+      if (filhoEsquerda != NULL) this->filhoEsquerda->mostrarPre();
+      cout << this->nome << " : " << this->idade  << endl;
+      if(filhoDireita != NULL) this->filhoDireita->mostrarPre();
+    };
+
+    void mostrarPos(){
+      if (filhoEsquerda != NULL) this->filhoEsquerda->mostrarPre();
+      if(filhoDireita != NULL) this->filhoDireita->mostrarPre();
+      cout << this->nome << " : " << this->idade  << endl;
+    };
+
+    void deletar(){
+      if (this->filhoEsquerda != NULL) this->filhoEsquerda->deletar();
+      if (this->filhoDireita != NULL) this->filhoDireita->deletar();
+      delete this;
+    };
+
+    Pessoa * busca(int idade){
+      if (this->idade == idade) return this;
+      if(this->idade > idade && this->filhoEsquerda != NULL) return this->filhoEsquerda->busca(idade);
+      if(this->idade < idade && this->filhoDireita != NULL) return this->filhoDireita->busca(idade);
+      return NULL;
+    };
+
+    void apaga(int idade){
+      //------------------------folha---------------------------------------
+      Pessoa* aux = this->busca(idade);
+      if(aux->filhoDireita == NULL && aux->filhoEsquerda == NULL) {
+        
+        if(aux->pai->filhoDireita == aux) aux->pai->filhoDireita = NULL;
+        if(aux->pai->filhoEsquerda == aux) aux->pai->filhoEsquerda = NULL;
+        delete aux;
+      }
+      // ---------------------com um filho----------------------------------
+      else if(aux->filhoDireita != NULL && aux->filhoEsquerda == NULL){
+        if(aux->pai->filhoDireita == aux) aux->pai->filhoDireita = aux->filhoDireita;
+        if(aux->pai->filhoEsquerda == aux) aux->pai->filhoEsquerda = aux->filhoDireita;
+        aux->filhoDireita->pai= aux->pai;
+        delete aux;
+      }else if(aux->filhoEsquerda != NULL && aux->filhoDireita == NULL){
+        if(aux->pai->filhoDireita == aux) aux->pai->filhoDireita = aux->filhoEsquerda;
+        if(aux->pai->filhoEsquerda == aux) aux->pai->filhoEsquerda = aux->filhoEsquerda;
+        aux->filhoEsquerda->pai = aux->pai;
+        delete aux;
+      }
+      // ---------com muitos filhos indo pelo descendente a direita---------
+      else {
+        //buscar descendente mais proximo
+        Pessoa* auxExtra = aux->filhoDireita;
+        while(auxExtra->filhoEsquerda != NULL ){
+          auxExtra = auxExtra->filhoEsquerda;
+        }
+        if(auxExtra->filhoDireita != NULL){
+          auxExtra->filhoDireita->pai = auxExtra->pai;
+        }
+        auxExtra->pai->filhoEsquerda = auxExtra->filhoDireita;
+        auxExtra->pai = aux->pai;
+        auxExtra->filhoDireita = aux->filhoDireita;
+        auxExtra->filhoEsquerda = aux->filhoEsquerda;
+        delete aux;
       }
 
     };
 
-    void mostrarPre(){
-
-      cout << this->nome << " : " << this->idade  << endl;
-
-      if (filhoEsquerda != NULL)
-        this->filhoEsquerda->mostrarPre();
-
-      if(filhoDireita != NULL)
-        this->filhoDireita->mostrarPre();
-
-    };
-
-    void deletar(){
-
-      if (this->filhoEsquerda != NULL)
-        this->filhoEsquerda->deletar();
-
-      if (this->filhoDireita != NULL)
-        this->filhoDireita->deletar();
-
-      delete this;
-    }
-
 };
+
 
 
 // class PessoaFisica:public Pessoa{
--- Pessoas/mainPessoa.cpp
+++ Pessoas/mainPessoa.cpp
@@ -4,34 +4,35 @@ int main(int argc, char *argv[]) {
 
   Pessoa* raiz = NULL;
   Pessoa* nova;
+  Pessoa* aux;
 
-  string inserir = "sim";
+  int inserir = 0;
   string nome;
   int idade;
 
-  while (inserir != "não"){
-
-    cout << "Insira o nome da pessoa." << endl;
+  while (inserir < 5){
+    cout << "--------------------------------------------------- " << endl;
+    cout << "Insira o nome da pessoa e idade" << endl;
     cin >> nome;
-    cout << " Inserir idade" << endl;
     cin >> idade;
 
     nova = new Pessoa(nome, idade);
     //cout << nova->getNome() << endl;
-
-    if (raiz==NULL){
-      raiz=nova;
-    }else{
-      raiz->inserirPessoa(nova);
-    }
-
-    cout << "Caso não deseje inserir nova pessoa digite 'não' " << endl;
-    cin >> inserir;
-
+    if (raiz==NULL)  raiz=nova;
+    else  raiz->inserirPessoa(nova);
+    inserir ++;
   };
 
-  raiz->mostrarPre();
-
+  //raiz->mostrarPre();
+  // raiz->mostrarPos();
+  cout << "----------Antes---------" << endl;
+  raiz->mostrarOrdem();
+  // aux = raiz->busca(5);
+  // cout << aux->nome << " tem " << aux->idade << endl;
+  raiz->apaga(40);
+  cout << "---------Depois---------" << endl;
+  cout << "Apagou quem tem 40" << endl;
+  raiz->mostrarOrdem();
   raiz->deletar();
 
 
