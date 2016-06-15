d28ffa421002b29947220cd063671b60258f0277
ArthurLuz <arthurluz17@uft.edu.br>
2016-06-15 20:50:58 -0300

    todos

--- Ordenação/main.cpp
+++ Ordenação/main.cpp
@@ -4,31 +4,56 @@
 int main(int argc, char const *argv[]) {
   Ordenar vet;
 
-  cout<<"Vetor 1:"<<endl;
-  vet.mostrar();
-  vet.bubbleSort();
-  cout<<endl;
-  cout<<"Vetor 1 ordenado com Bubble Sort:"<<endl;
-  vet.mostrar();
-  cout<<"___________________________________________________________________"<<endl<<endl;
+  // cout<<"Vetor 1:"<<endl;
+  // vet.mostrar();
+  // vet.bubbleSort();
+  // cout<<endl;
+  // cout<<"Vetor 1 ordenado com Bubble Sort:"<<endl;
+  // vet.mostrar();
+  // cout<<"___________________________________________________________________"<<endl<<endl;
+  //
+  // cout<<"Vetor 2:"<<endl;
+  // vet.aleatorio();
+  // vet.mostrar();
+  // vet.selectionSort();
+  // cout<<endl;
+  // cout<<"Vetor 2 ordenado com Selection Sort:"<<endl;
+  // vet.mostrar();
+  // cout<<"___________________________________________________________________"<<endl<<endl;
+  //
+  // cout<<"Vetor 3:"<<endl;
+  // vet.aleatorio();
+  // vet.mostrar();
+  // vet.insertionSort();
+  // cout<<endl;
+  // cout<<"Vetor 3 ordenado com Insertion Sort:"<<endl;
+  // vet.mostrar();
+  // cout<<"___________________________________________________________________"<<endl<<endl;
+  //
+  // cout<<"Vetor 4:"<<endl;
+  // vet.aleatorio();
+  // vet.mostrar();
+  // vet.shellSort();
+  // cout<<endl;
+  // cout<<"Vetor 4 ordenado com Shell Sort:"<<endl;
+  // vet.mostrar();
+  // cout<<"___________________________________________________________________"<<endl<<endl;
 
-  cout<<"Vetor 2:"<<endl;
+  cout<<"Vetor 5:"<<endl;
   vet.aleatorio();
   vet.mostrar();
-  vet.selectionSort();
+  vet.chamarQuicksort();
   cout<<endl;
-  cout<<"Vetor 2 ordenado com Selection Sort:"<<endl;
+  cout<<"Vetor 5 ordenado com quickSort:"<<endl;
   vet.mostrar();
   cout<<"___________________________________________________________________"<<endl<<endl;
 
-  cout<<"Vetor 3:"<<endl;
+  cout<<"Vetor 6:"<<endl;
   vet.aleatorio();
   vet.mostrar();
-  vet.insertioSort();
+  vet.chamarMergeSort();
   cout<<endl;
-  cout<<"Vetor 3 ordenado com Selection Sort:"<<endl;
+  cout<<"Vetor 6 ordenado com MergeSort:"<<endl;
   vet.mostrar();
   cout<<"___________________________________________________________________"<<endl<<endl;
-
-
 }
--- Ordenação/ordenar.hpp
+++ Ordenação/ordenar.hpp
@@ -8,30 +8,27 @@ using namespace std;
 class Ordenar{
 private:
   int t;
-  int V[tamanho];
+  int v[tamanho];
 
 public:
 
   Ordenar() {
     t= tamanho;
-    srand(time(NULL));
-    for(int i=0; i<t ;i++){
-      V[i]=1+(rand()%100);
-    }
+    aleatorio();
   };
 
   ~Ordenar(){};
 
   void mostrar(){
     for(int i=0; i< t; i++){
-      cout << "Valor na posição "<< i <<" é: " << V[i] << endl;
+      cout << "Valor na posição "<< i <<" é: " << v[i] << endl;
     }
   };
 
   void aleatorio(){
     srand(time(NULL));
     for(int i=0; i<t ;i++){
-      V[i]=1+(rand()%100);
+      v[i]=1+(rand()%100);
     }
   };
 
@@ -39,10 +36,10 @@ public:
     /* Compara de dois em dois numeros e faz a troca entre eles se necessario. */
     for(int i=0; i<t; i++){
       for(int j=0; j<t; j++){
-        if(V[i]<V[j]){
-          int aux =V[j];
-          V[j]=V[i];
-          V[i]=aux;
+        if(v[i]<v[j]){
+          int aux =v[j];
+          v[j]=v[i];
+          v[i]=aux;
         }
       }
     }
@@ -51,31 +48,110 @@ public:
   void selectionSort() {
     /* Ordena por posição. Varre o vetor e seleciona o menor numero e o insere na posição que esta sendo ordenada. */
     for(int i=0; i<t;i++){
-      int aux=V[i];
+      int aux=v[i];
       for(int j=i+1 ; j<t; j++){
-        if(aux>V[j]){
-          int aux2=V[j];
-          V[j]=aux;
+        if(aux>v[j]){
+          int aux2=v[j];
+          v[j]=aux;
           aux=aux2;
         }
       }
-      V[i]=aux;
+      v[i]=aux;
     }
   };
 
-  void insertioSort(){
+  void insertionSort(){
     /* Compara um trexo ja ordenado com o proximo elemento e entao o ordena*/
-    int tOrd=1;//variavel que indica o tamanho do vetor q ja esta ordenado.
-    for(int i=(tOrd-1); i<=0;i--){
-        if(V[tOrd]<V[i]){
-          int aux=V[j];
-          V[j]=V[tOrd];
-          V[tOrd]=aux;
+    for(int i=1; i<t ; i++){
+        int aux=v[i];
+        int j=i-1;
+        while(j>=0 && v[j]>aux){
+          v[j+1]=v[j];
+          j--;
         }
-        tOrd++;
+        v[j+1]=aux;
     }
+  };
 
+  void shellSort(){
+    /* Usa uma chave pra diminuir a quantidade de trocas*/
+    int metade = t/2;
+    while(metade > 0){
+      for(int i = 0; i < t-metade; i++){
+        if(v[i] > v[i+metade]){
+          int aux = v[i];
+          v[i] = v[i+metade];
+          v[i+metade] = aux;
+        }
+      }
+      metade--;
+    }
+  };
 
+  void chamarMergeSort(){
+    mergeSort(0,t);
+  };
+  void mergeSort( int inicio, int fim) {
+    if(inicio == fim) return;
+    int meio = (inicio + fim ) / 2;
+    mergeSort(inicio, meio);
+    mergeSort(meio + 1, fim);
+    int k=0, i = inicio, j = meio + 1;
+    int *temporario = new int [fim - inicio + 1];
+    while(i < meio + 1 || j  < fim + 1) {
+        if (i == meio + 1 ) {
+            temporario[k] = v[j];
+            j++;
+            k++;
+        }
+        else {
+            if (j == fim + 1) {
+                temporario[k] = v[i];
+                i++;
+                k++;
+            }
+            else {
+                if (v[i] < v[j]) {
+                    temporario[k] = v[i];
+                    i++;
+                    k++;
+                }
+                else {
+                    temporario[k] = v[j];
+                    j++;
+                    k++;
+                }
+            }
+        }
+    }
+    for(i = inicio; i <= fim; i++) {
+        v[i] = temporario[i - inicio];
+    }
+    delete [] temporario;
+}
+
+  void chamarQuicksort() {
+    quickSort(0,t);
+    cout<<"chamou"<<endl;
+  };
+
+  void quickSort(int inicio, int fim){
+     /* Usa recursividade */
+     int pivor = v[inicio];
+     int i=inicio, j=fim, aux;
+     while (i<=j) {
+       while(v[i]<pivor && i<fim){ i++; }
+       while(v[j]>pivor && j>inicio){ j--; }
+       if(i<=j){
+         aux = v[i];
+         v[i]=v[j];
+         v[j]= aux;
+         i++;
+         j--;
+       }
+     }
+     if(j>inicio)       quickSort(inicio,j);
+     if(i<fim)    quickSort(i,fim);
   };
 
 };
